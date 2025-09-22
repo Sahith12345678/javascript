e3e06379fd0bc95564324bb0786dfef6dad7058b
sahith12345678 <thipparapuhnumanth@gmail.com>
2025-09-22 11:02:02 +0530

    datatype

--- app.js
+++
@@ -1,5 +0,0 @@
-var a = 67;
-var a = 90;
-console.log(a);
-let b = 68;
-console.log(`value b is ${b}`);
---
+++ datatype.js
@@ -0,0 +1,39 @@
+var a = 67;
+var a = 90;
+console.log(a);
+let b = 68;
+console.log(`value b is ${b}`);
+//comment
+/* multi line comment */
+// hello how are you
+//1.primitive 2. non-primitive
+// primitive
+// 1. number
+// 2. string
+// 3. boolean
+// 4. undefined
+// 5. null
+// 6. symbol
+// 7. bigint
+
+//2. non-primitive
+// 1. object
+// 2. array
+
+a = 8;
+console.log(typeof a); //for type of data
+let userName = "sahith";
+console.log(typeof userName);
+let c = true;
+console.log(typeof c);
+let data = null;
+console.log(typeof data);
+data = undefined;
+console.log(typeof data);
+
+//non-primitive
+let array = [1, "john", true, null];
+console.log(typeof array);
+
+data = { name: "sahith", age: 21, dept: "cse" };
+console.log(typeof data);
